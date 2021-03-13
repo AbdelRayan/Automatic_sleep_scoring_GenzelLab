696074ac5282093c5c3a94c8e2287a561f1e8bc1
abelrayan <abdelrahman.n.rayan@gmail.com>
2021-03-13 17:33:36 +0100

    Downsamped the data and tried HMM again

--- Acceleration_Data_Curation.m
+++ Acceleration_Data_Curation.m
@@ -21,17 +21,21 @@ for jj=1:length(patIDacc)
     ACC_RES = [ACC_RES  Dataacc]; 
 end
 disp('Finished reading the accelerometer data')
+%% working with downsampled data to reduce noise 
+ACC_RES1 = decimate(ACC_RES(:,1),200,'FIR');
+ACC_RES2 = decimate(ACC_RES(:,2),200,'FIR');
+ACC_RES3 = decimate(ACC_RES(:,3),200,'FIR');
 %% important parameters for further computation 
 Ts      = mean(diff(TimeVect)); % this the period 
 Fs      = 1/Ts;                 % the sampling frequency
 %% extracting the x, y and z coordinates 
-g_x     = ACC_RES(:,1);
-g_y     = ACC_RES(:,2);
-g_z     = ACC_RES(:,3);
+g_x     = ACC_RES1;%(:,1);
+g_y     = ACC_RES2;%(:,2);
+g_z     = ACC_RES3;%(:,3);
 %% calculate the g-level norm
 g = sqrt(g_x.^2 + g_y.^2 + g_z.^2);
 %% performin a Welch power spectrum analysis of the g-norm signal 
-[accPow,f]=pwelch(g,[],[],[1:0.1:10],Fs); % because I did not downsample the signal so I am limiting the frequency range 
+[accPow,f]=pwelch(g,[],[],[1:0.1:50],100); % because I did not downsample the signal so I am limiting the frequency range 
 % from the power spectrum, I notice a fundmental frequency up 3 Hz
 %% filtering the g-norm signal I am following the method from Carr et al in Nature 
 % Zero-Delay Filter Options for G-level filtering
@@ -50,7 +54,7 @@ g_z_filt = filtfilt(d1,g_z);
 g_filt = sqrt(g_x_filt.^2 + g_y_filt.^2 + g_z_filt.^2);
 %% I will be trying to use HMM to classify sleep and awake 
 
-dataMat = [g g_filt];
+dataMat = [NewAcceleration envelopes'];
 % The amplitude envelope is computed using the hilbert transform
 envelopes = abs(hilbert(dataMat)');
 
@@ -63,7 +67,7 @@ threshold = 2*median(envelopes,2);
     options.K = 2;
     options.standardise = 0;
     options.verbose = 1;
-    options.Fs = Fs;
+    options.Fs = 100;
     options.useMEX = 1;
     options.zeromean = 0;
     options.dropstates = 1;
@@ -79,9 +83,20 @@ NewAcceleration = locdetrend(g_filt,20000,[.1 .05]);
 envelopes = abs(hilbert(NewAcceleration)');
 %%
 
-% plot(envelopes)
+plot(NewAcceleration)
 % histogram(envelopes,0:0.01:2)
-threshold = 2*median(zscore(NewAcceleration));
-plot(zscore(NewAcceleration))
+% threshold = 2*median(zscore(NewAcceleration));
+% plot(NewAcceleration(1:5000000))
 hold on
-yline(threshold,'r--','LineWidth',2)
+yline(threshold(1),'r--','LineWidth',2)
+yline(-threshold(1),'r--','LineWidth',2)
+%%
+area(1:1:T,Gamma_emb{1})
+hold on
+plot(1:1:T,NewAcceleration*10,'k','LineWidth',2)
+
+
+xlim([1 90000])
+%%
+matDecode = Gamma_emb{1};
+area(matDecode)
